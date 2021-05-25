2a55441ba694d3f27ec15bc275b42cf62f7dfe82
Thomas Gaylard Admin <tbg-beanstalk@genonline.co.uk>
2021-05-25 15:08:09 +0100

    Commit after adding simple database functionality

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from html_utilities import table
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reminders.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username!r}>'
 
 
-@app.route('/mail')
+class ReminderAlert(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(300), nullable=False)
+    issue_time = db.Column(db.DateTime(), nullable=False)
+
+    def __repr__(self):
+        return f'<ReminderAlert {id}: {self.message!r}: {self.issue_time!r}'
+
+
+@app.route('/users')
+def list_users():
+    users = User.query.all()
+    users_html = table(['username', 'email'], users)
+    return users_html
+
+
+@app.route('/alerts')
 def process_mail():
-    return "You have mail"
+    alerts = ReminderAlert.query.all()
+    alerts_html = table(['id', 'message', 'issue_time'], alerts)
+    return alerts_html
 
 
 if __name__ == '__main__':
---
+++ html_utilities.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+
+def table(headings, records):
+    """
+    Creates a table with a row of headings and then one row for each database record passed in.
+
+    :param headings: List of field names to use as headers
+    :param records: Iterable of database records with fields corresponding to field names in headers
+    :return:
+    """
+
+    heading_html = '<tr>' + reduce(lambda header_html, item: header_html + f'<th>{item}</th>', headings, '') + '</tr>'
+
+    html = '<table>'
+    html += heading_html
+
+    for record in records:
+        row = '<tr>'
+        for field_name in headings:
+            field_value = getattr(record, field_name)
+            field_html = f'<td>{field_value}</td>'
+            row += field_html
+        row += '</tr>'
+        html += row
+
+    html += '</table>'
+
+    return html
