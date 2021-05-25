ee42d808629e2ab1232c4f2bdd017eebc6ca8eac
Thomas Gaylard Admin <tbg-beanstalk@genonline.co.uk>
2021-05-25 12:29:09 +0100

    Go back to simple starter app as getting confused trying to mix code from different tutorials.

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
 from flask import Flask
-from flask import current_app
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
 
 
-def init_app():
-    """Construct the core application."""
-    new_app = Flask(__name__, instance_relative_config=False)
-    new_app.config.from_object('config.Config')
-
-    db.init_app(new_app)
-
-    with new_app.app_context():
-        # from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
-
-        return new_app
-
-
-@current_app.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
-@current_app.route('/mail')
+@app.route('/mail')
 def process_mail():
     return "You have mail"
 
 
 if __name__ == '__main__':
-    app = init_app()
     app.run()
